6ac2a55c1c5aeb05c28ba57acd754a4ae1883599
Mr.Egor <leidark777@gmail.com>
2026-07-11 01:43:54 +0300

    Create D.cpp

---
+++ Динамическое программирование [3]/D.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+const int MOD = 1000000007;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<vector<bool>> danger(n + 1, vector<bool>(m + 1, false));
+    for (int i = 0; i < k; ++i) {
+        int x, y;
+        cin >> x >> y;
+        danger[x][y] = true;
+    }
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    dp[0][0] = 1;
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j <= m; ++j) {
+            if (i == 0 && j == 0) continue;
+            if (danger[i][j]) {
+                dp[i][j] = 0;
+            } else {
+                int ways = 0;
+                if (i > 0) ways = (ways + dp[i - 1][j]) % MOD;
+                if (j > 0) ways = (ways + dp[i][j - 1]) % MOD;
+                dp[i][j] = ways;
+            }
+        }
+    }
+    cout << dp[n][m] << '\n';
+    return 0;
+}
