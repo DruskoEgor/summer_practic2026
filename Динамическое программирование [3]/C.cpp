e7dbae8975feb407c6180278c6369a4dcf2c00c3
Mr.Egor <leidark777@gmail.com>
2026-07-11 01:41:10 +0300

    Create C.cpp

---
+++ Динамическое программирование [3]/C.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+const int MOD = 1000000007;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int l, r;
+    cin >> l >> r;
+    vector<int> dp(r + 1, 0);
+    dp[1] = 1;
+    for (int i = 1; i <= r; ++i) {
+        if (dp[i] == 0) continue;
+        for (int j = 2 * i; j <= r; j += i) {
+            dp[j] += dp[i];
+            if (dp[j] >= MOD) dp[j] -= MOD;
+        }
+    }
+    long long ans = 0;
+    for (int i = l; i <= r; ++i) {
+        ans += dp[i];
+        if (ans >= MOD) ans -= MOD;
+    }
+    cout << ans % MOD << '\n';
+    return 0;
+}
