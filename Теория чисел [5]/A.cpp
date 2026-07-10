a6e9e06662e53aa99a165606dc15bf763a0513c4
Mr.Egor <leidark777@gmail.com>
2026-07-11 01:54:33 +0300

    Create A.cpp

---
+++ Теория чисел [5]/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    long long ans = 1;
+    for (int i = 0; i < n; ++i) {
+        long long x;
+        cin >> x;
+        ans = ans / gcd(ans, x) * x;
+    }
+    cout << ans << '\n';
+    return 0;
+}
